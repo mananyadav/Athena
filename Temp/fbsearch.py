64ffa550b30eed642743ed981282d706a1c85cfa
mananyadav <manan.yadav02@gmail.com>
2016-12-12 03:14:46 +0530

    Facebook search added somewhat

---
+++ Temp/fbsearch.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import webbrowser
+
+def search(arg):
+	print 'Searching for ' + arg + '...'
+	webbrowser.open('http://www.facebook.com/search/top/?q='+arg,new=2)
+
+inp = raw_input('Enter name : ')
+search(inp)
+	
