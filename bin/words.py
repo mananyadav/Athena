8f2cfa6251181a305a8c883ca484475e8005ec62
mananyadav <manan.yadav02@gmail.com>
2016-12-12 14:37:16 +0530

    bin/words.py has had a majore breakthrough!

--- bin/words.py
+++ bin/words.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python
 import datetime
 from datetime import datetime
-import datetime
 import wikipedia
+import os
+def main():
+	running = True
+	while running:
+		pr = {}
+		userInput = raw_input('=>')
+		words = userInput.split()
+		a = 0
+		for i in words:
+			pr[a] = i
+			a += 1
 
-pr = {}
-userInput = raw_input('-->')
-words = userInput.split()
-a = 0
-for i in words:
-	pr[a] = i
-	a += 1
+		for i in range(0, len(pr)):
+			# print pr[i]
+			if pr[i] in ['date', 'date?']:
+				print datetime.now().strftime('Today is %d-%m-%Y')
+			elif pr[i] in ['time', 'time?']:
+				print datetime.now().strftime('The time is %H:%M %p')
+			elif pr[i] in ['exit']:
+				running = False
+			elif pr[i] in ['clear']:
+				os.system('clear')
+			# elif pr[i] == 'who':
+			# 	if pr[i+1] == 'is':
+			# 		person = pr[i+2:]
+			# 		wikipedia.summary(person)
+			elif userInput[:7] in ['who is ']:
+				person = userInput[7:]
+				print 'Searching for ' + person + '...'
+				infoPerson = wikipedia.summary(person,sentences=1)
+				print infoPerson
+				break
 
-for i in range(0, len(pr)):
-	# print pr[i]
-	if pr[i] in ['date', 'date?']:
-		today = datetime.date.today()
-		print today.strftime('The date is %d, %b %Y')
-	elif pr[i] in ['time', 'time?']:
-		print datetime.now().strftime('%H:%M %p')
-	# elif pr[i] == 'who':
-	# 	if pr[i+1] == 'is':
-	# 		person = pr[i+2:]
-	# 		wikipedia.summary(person)
-	# elif userInput[:7] in ['who is ']:
-	#	person = userInput[7:]
-	#	print 'Searching for ' + person + '...'
-	#	infoPerson = wikipedia.summary(person,sentences=1)
-	#	print infoPerson
-exit()
+if __name__ == '__main__':
+	main()
