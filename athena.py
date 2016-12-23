39027d87dae04c3d13efbb387e3de24514c5261e
mananyadav <manan.yadav02@gmail.com>
2016-12-24 00:07:14 +0530

    made athena.py to a standlone module

--- athena.py
+++ athena.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python
 __author__ = 'Manan Yadav'
-import os
-from bin import words
-import random
-import speech_recognition as sr
 
-word = sr.Recognizer()
-os.system('clear')
-with sr.Microphone() as source:
-	os.system('clear')
-	print("Listening")
-	audio = word.listen(source)
-try:
+def main():
+	import os
 	from bin import words
-	userInput = word.recognize_google(audio)
-	print("User : " + userInput)
-	words.main(str(userInput))
-except sr.UnknownValueError:
-	speak.speak("")
+	import speak
+	import speech_recognition as sr
 
-os.system('python athena.py')
+	word = sr.Recognizer()
+	os.system('clear')
+	with sr.Microphone() as source:
+		os.system('clear')
+		print("Listening")
+		audio = word.listen(source)
+	try:
+		from bin import words
+		userInput = word.recognize_google(audio)
+		print("User : " + userInput)
+		words.main(str(userInput))
+	except sr.UnknownValueError:
+		pass
+
+	os.system('python athena.py')
+
+if __name__ == '__main__':
+	main()
--- media/README.md
+++ media/README.md
@@ -5,3 +5,5 @@
 + Alan Walker - Fade: https://www.youtube.com/watch?v=bM7SZ5SBzyY
 
 + Jim Yosef - Arrow: https://www.youtube.com/watch?v=5LI5QZVUU98
+
+#### Place your music in the music/ directory.
