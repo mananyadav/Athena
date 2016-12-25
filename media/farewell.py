7389677b5f0661023131dcd60d5b4f095aa04d98
mananyadav <manan.yadav02@gmail.com>
2016-12-25 17:18:09 +0530

    added some files to media/

---
+++ media/farewell.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+goodByeWords = {
+	1: 'Bye!',
+	2: 'See you later!',
+	3: 'Goodbye, sir.',
+	4: 'Finally.',
+	5: 'Yay. Time away from you!',
+}
---
+++ media/greetings.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+greetings = {
+	1: 'Hello, sir.',
+	2: 'Nice being back, sir.',
+	3: 'Good to be back, sir.',
+}
