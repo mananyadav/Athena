c3f6e897d972f443a72deb1a7e4535e70bf307a9
mananyadav <manan.yadav02@gmail.com>
2017-01-01 16:47:32 +0530

    made more userInfo options

--- bin/data/userInfo.py
+++ bin/data/userInfo.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
-name = ''
-age = 0
-dateOfBirth = 'xx-xx-20xx' # format - dd-mm-yy
-emailID = 'example@example.com'
-socialNetworks = {
-	'facebook': False,
-	'twitter': False,
-	'instgram': False,
-}
+class user():
+	name = ''
+	age = 0
+	dateOfBirth = 'xx-xx-20xx' # format - dd-mm-yy
+	emailID = 'example@example.com'
+	socialNetworks = {
+		'facebook': False,
+		'twitter': False,
+		'instagram': False,
+	}
+	gender = ''
