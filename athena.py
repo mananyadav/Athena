6dc6a492a28e40673d193013870c1cd27cc5caff
mananyadav <manan.yadav02@gmail.com>
2017-01-07 03:04:52 +0530

    fixed social net bug AGAIN!

--- athena.py
+++ athena.py
@@ -9,24 +9,24 @@ def main():
     from bin import words
     import speech_recognition as sr
 
-    try:
-        word = sr.Recognizer()
+    # try:
+    word = sr.Recognizer()
+    os.system('clear')
+    with sr.Microphone() as source:
         os.system('clear')
-        with sr.Microphone() as source:
-            os.system('clear')
-            print("Listening...")
-            audio = word.listen(source)
-        try:
-            from bin import words
-            userInput = word.recognize_google(audio)
-            print("I heard : " + userInput)
-            words.main(str(userInput))
-        except sr.UnknownValueError:
-            pass
+        print("Listening...")
+        audio = word.listen(source)
+    try:
+        from bin import words
+        userInput = word.recognize_google(audio)
+        print("I heard : " + userInput)
+        words.main(str(userInput))
+    except sr.UnknownValueError:
+        pass
 
         os.system('python athena.py')
-    except Exception:
-        pass
+    # except Exception:
+    #    pass
 
 if __name__ == '__main__':
     main()
